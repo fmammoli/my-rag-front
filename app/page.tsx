7c7013da2075cb3500acdb363fbe0661902813bc
Felipe Mammoli <fmammoli@gmail.com>
2024-11-19 23:31:39 -0300

    token fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,8 @@ export default function Home() {
   return (
     <div className={"h-[100svh] w-[100svw]"}>
       <h1>Pouso Alegre - Plano Diretor Interativo</h1>
-      <div className="flex gap-4 h-full">
-        <MyMap></MyMap>
-      </div>
+
+      <MyMap></MyMap>
     </div>
   );
 }
